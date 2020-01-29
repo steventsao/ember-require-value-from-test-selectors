251912415157340311405f46fffabc8b53dc0d5d
Steven Tsao <stsao@stsao-ld1.linkedin.biz>
2020-01-29 11:30:41 -0800

    Remove process and report calls. Update Element node to select only data-test tags

--- transforms/data-test-add-value/index.js
+++ transforms/data-test-add-value/index.js
@@ -2,49 +2,24 @@ const { getParser } = require('codemod-cli').jscodeshift;
 const { getOptions: getCLIOptions } = require('codemod-cli');
 const recast = require('ember-template-recast');
 
-function process(node) {
-  if (
-    (node.type === 'MustacheStatement' || node.type === 'BlockStatement') &&
-    node.params &&
-    node.params.length
-  ) {
-    node.params.forEach(param => {
-      if (param.original && param.original.includes('data-test')) {
-        this._report(node);
-      }
-    });
-  }
-  return false;
-}
-function _report(node) {
-  this.log({
-    message: ERROR_NAMESPACE_MESSAGE,
-    line: node.loc && node.loc.start.line,
-    column: node.loc && node.loc.start.column,
-    source: this.sourceForNode(node),
-  });
-}
-
 module.exports = function transformer(file, api) {
   let { code } = recast.transform(file.source, env => {
     let { builders: b } = env.syntax;
     return {
       ElementNode(node) {
-        // for (let i = 0; i < node.attributes.length; i++) {
-        //   let attribute = node.attributes[i];
-        //   if (
-        //     attribute.value &&
-        //     attribute.value.type === 'TextNode' &&
-        //     attribute.value.chars === ''
-        //   ) {
-        //     attribute.value.chars = '="true"';
-        //   }
-        // }
+        for (let i = 0; i < node.attributes.length; i++) {
+          let attribute = node.attributes[i];
+          if (
+            attribute.value &&
+            attribute.value.type === 'TextNode' &&
+attribute.name.includes('data-test') &&
+            attribute.value.chars === ''
+          ) {
+            attribute.value.chars = '="true"';
+          }
+        }
       },
 
-      //   ComponentNode(node) {
-      //     process(node);
-      //   },
 
       MustacheStatement(node) {
         if (
@@ -61,7 +36,6 @@ module.exports = function transformer(file, api) {
               param.original.includes('data-test')
             ) {
               node.hash.pairs.unshift(b.pair(param.original, b.boolean(true)));
-              //   console.log(node);
             }
           });
           node.params = node.params.filter(
@@ -71,25 +45,25 @@ module.exports = function transformer(file, api) {
       },
 
       BlockStatement(node) {
-        // if (
-        //   (node.type === 'MustacheStatement' || node.type === 'BlockStatement') &&
-        //   node.params &&
-        //   node.params.length
-        // ) {
-        //   node.params.forEach(param => {
-        //     if (
-        //       param.original &&
-        //       typeof param.original === 'string' &&
-        //       param.original.includes('data-test')
-        //     ) {
-        //       node.hash.pairs.push(b.pair(param.original, b.boolean(true)));
-        //       //   console.log(node);
-        //     }
-        //   });
-        // }
+         if (
+           (node.type === 'MustacheStatement' || node.type === 'BlockStatement') &&
+           node.params &&
+           node.params.length
+         ) {
+           node.params.forEach(param => {
+             if (
+               param.original &&
+               typeof param.original === 'string' &&
+               param.original.includes('data-test')
+             ) {
+               node.hash.pairs.push(b.pair(param.original, b.boolean(true)));
+             }
+           });
+         }
       },
     };
   });
   return code;
 };
+
 //   https://github.com/ember-codemods/ember-angle-brackets-codemod/blob/master/transforms/angle-brackets/index.js
